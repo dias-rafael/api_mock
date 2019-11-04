13f44a4eac5743793ff5f0a212d6147cbfcb5e9b
Rafael Dias <rdias@2mundos.net>
2019-11-04 16:13:27 -0300

    Correcao exibicao json

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api', function(req, res){
         var response = {status: 'falha', resultado: err};
         res.json(response);
       } else {
-        res.send(JSON.parse(data));
+        res.send(JSON.parse(data).info);
         /*
         var obj = JSON.parse(data);
         var result = 'Nenhuma informação foi encontrada';
